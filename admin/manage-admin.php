607dbff1b0a048ac7411e6caadbf55d2479952e9
masum-mushfiq <67286805+masum-mushfiq@users.noreply.github.com>
2021-10-31 22:08:11 +0600

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,63 @@
+<?php include 'partials/menu.php'; ?>
+
+        <!-- Main Content Section Starts Here -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+                <br /><br />
+
+                <!-- Button to add admin -->
+                <a href="#" class="btn-primary">Add Admin</a>
+                <br /><br />
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>Serial No.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Masum Mushfiq</td>
+                        <td>masummushfiq</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                        
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Masum Mushfiq</td>
+                        <td>masummushfiq</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                        
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Masum Mushfiq</td>
+                        <td>masummushfiq</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                        
+                    </tr>
+                </table>
+
+                
+
+                <div class="clearfix"></div>
+            </div>
+            
+        </div>
+        <!-- Main Content Section Ends Here -->
+
+<?php include 'partials/footer.php'; ?>        
---
+++ admin/partials/footer.php
@@ -0,0 +1,11 @@
+ <!-- Footer Section Starts Here -->
+ <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All right reserved, Food House. Developed By -<a href="https://www.linkedin.com/in/md-masum-mushfiq-9a82ab222/" target="_blank">Masum Mushfiq</a></p>
+            </div>
+            
+        </div>
+        <!-- Footer Section Ends Here -->
+    </body>
+    
+</html>
