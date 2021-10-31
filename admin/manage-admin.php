075e393014ed19252fcc798703648b737816fd6e
masum-mushfiq <67286805+masum-mushfiq@users.noreply.github.com>
2021-10-31 23:40:08 +0600

    Done

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,10 +4,12 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Admin</h1>
+
                 <br /><br />
 
                 <!-- Button to add admin -->
-                <a href="#" class="btn-primary">Add Admin</a>
+                <a href="add-admin.php" class="btn-primary">Add Admin</a>
+
                 <br /><br />
 
                 <table class="tbl-full">
@@ -30,23 +32,23 @@
                     </tr>
 
                     <tr>
-                        <td>1.</td>
+                        <td>2.</td>
                         <td>Masum Mushfiq</td>
                         <td>masummushfiq</td>
                         <td>
-                            Update Admin
-                            Delete Admin
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
                         </td>
                         
                     </tr>
 
                     <tr>
-                        <td>1.</td>
+                        <td>3.</td>
                         <td>Masum Mushfiq</td>
                         <td>masummushfiq</td>
                         <td>
-                            Update Admin
-                            Delete Admin
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
                         </td>
                         
                     </tr>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -5,6 +5,52 @@ include '<partials/menu.php'; ?>
 <div class="main-content">
     <div class="wrapper">
          <h1>Manage Order</h1> 
+
+         
+
+<br /><br />
+
+<table class="tbl-full">
+    <tr>
+        <th>Serial No.</th>
+        <th>Full Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+    </tr>
+
+    <tr>
+        <td>1.</td>
+        <td>Masum Mushfiq</td>
+        <td>masummushfiq</td>
+        <td>
+            <a href="#" class="btn-secondary">Update Admin</a>
+            <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+        
+    </tr>
+
+    <tr>
+        <td>2.</td>
+        <td>Masum Mushfiq</td>
+        <td>masummushfiq</td>
+        <td>
+            <a href="#" class="btn-secondary">Update Admin</a>
+            <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+        
+    </tr>
+
+    <tr>
+        <td>3.</td>
+        <td>Masum Mushfiq</td>
+        <td>masummushfiq</td>
+        <td>
+            <a href="#" class="btn-secondary">Update Admin</a>
+            <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+        
+    </tr>
+</table>
     </div>
     
 </div>
